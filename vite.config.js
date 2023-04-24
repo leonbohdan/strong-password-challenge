15370aeda483c974fc146116a7f06e3ea506cc30
bohdan leonets <bohdan@mytowerapp.com>
2023-04-24 09:30:19 +0300

    prepare for build

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
   server: {
     port: 8000
   },
+  base: '/strong-password-challenge/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
